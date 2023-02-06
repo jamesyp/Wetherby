d1d559e530b54a78ad2b92b871f43a5ff79c4361
James Patton <jamesyp@gmail.com>
2023-02-05 16:17:42 -0800

    feat: adds forecast lookup via NWS API (#6)
    * feat: adds forecast lookup via NWS API
    
    * refactor: simplify interactor error handling

--- app/interactors/validate_and_expand_address.rb
+++ app/interactors/validate_and_expand_address.rb
@@ -11,10 +11,7 @@ class ValidateAndExpandAddress < BaseInteractor
 
   private
 
-  def value
-    validated_address
-    return if error
-
+  def result
     {
       standardized_address: validated_address.dig(:usps_data, :standardized_address),
       geo_point: validated_address.dig(:geocode, :location)
@@ -23,18 +20,5 @@ class ValidateAndExpandAddress < BaseInteractor
 
   def validated_address
     @validated_address ||= AddressValidationClient.validate(street: @street, city: @city, state: @state)[:result]
-  rescue StandardError => e
-    @error = e.message
-    @validated_address = {}
-  end
-
-  def request_body
-    {
-      address: {
-        addressLines: [@street],
-        administrativeArea: @state,
-        locality: @city
-      }
-    }.to_json
   end
 end
---
+++ spec/interactors/get_weather_forecast_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe GetWeatherForecast do
+  subject(:result) { described_class.run(latitude:, longitude:) }
+
+  let(:latitude) { 37.7642911 }
+  let(:longitude) { -122.4660297 }
+
+  let(:request_headers) { { 'User-Agent' => 'Wetherby backend, contact: jamesyp@gmail.com' } }
+
+  let(:point_endpoint) { "https://api.weather.gov/points/#{latitude},#{longitude}" }
+  let(:point_response_fixture) { 'weather_gov/point_response.json' }
+  let(:point_response) { double(body: file_fixture(point_response_fixture).read, ok?: point_response_ok) }
+  let(:point_response_ok) { true }
+
+  let(:forecast_endpoint) { JSON.parse(point_response.body).dig('properties', 'forecast') }
+  let(:forecast_response_fixture) { 'weather_gov/forecast_response.json' }
+  let(:forecast_response) { double(body: file_fixture(forecast_response_fixture).read) }
+
+  let(:expected_forecast) do
+    JSON.parse(forecast_response.body).deep_transform_keys(&:underscore).deep_symbolize_keys.dig(:properties, :periods)
+  end
+
+  before do
+    allow(HTTParty).to receive(:get).with(point_endpoint, anything).and_return(point_response)
+    allow(HTTParty).to receive(:get).with(forecast_endpoint, anything).and_return(forecast_response)
+  end
+
+  it 'gets geopoint and forecast data using the National Weather Service API' do
+    expect(HTTParty).to receive(:get).with(
+      "https://api.weather.gov/points/#{latitude},#{longitude}",
+      headers: request_headers
+    )
+    expect(HTTParty).to receive(:get).with(
+      forecast_endpoint,
+      headers: request_headers
+    )
+
+    subject
+  end
+
+  it 'returns an array of forecast periods' do
+    expect(subject.result).to eq(expected_forecast)
+  end
+
+  context 'API returns an error' do
+    let(:point_response_fixture) { 'weather_gov/point_error_response.json' }
+    let(:point_response_ok) { false }
+    let(:error) { JSON.parse(point_response.body)['detail'] }
+
+    it do
+      expect(subject.success?).to eq(false)
+      expect(subject.result).to be_nil
+      expect(subject.error).to eq(error)
+    end
+  end
+end
