e4feffe48a84c6516972370bdafb4d023f39035b
James Patton <jamesyp@gmail.com>
2023-02-05 20:27:00 -0800

    feat: caches API responses (#9)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,19 +8,13 @@ Rails.application.configure do
   config.consider_all_requests_local = true
   config.server_timing = true
 
-  if Rails.root.join('tmp/caching-dev.txt').exist?
-    config.action_controller.perform_caching = true
-    config.action_controller.enable_fragment_cache_logging = true
+  config.action_controller.perform_caching = true
+  config.cache_store = :memory_store
 
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
-    }
-  else
-    config.action_controller.perform_caching = false
-
-    config.cache_store = :null_store
-  end
+  config.action_controller.enable_fragment_cache_logging = true
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, max-age=#{2.days.to_i}"
+  }
 
   config.active_support.deprecation = :log
   config.active_support.disallowed_deprecation = :raise
